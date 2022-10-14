f8bdae0a7009424d0a385b84aa9e3c43e93a7806
Motohiro_EStride <m.mitsuhashi@elemental-stride.com>
2022-10-15 05:25:41 +0900

    lint error 解消 及び初期設定

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,32 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 const Main = () => {
-  const { connectWallet, sendTransaction, handleChange, inputFormData } = useContext(TransactionContext);
+  const {
+    connectWallet, sendTransaction, handleChange, inputFormData,
+  } = useContext(TransactionContext);
   const handleSubmit = () => {
     const { addressTo, amount } = inputFormData;
-    if(addressTo === "" || amount === "") {
+    if(addressTo === '' || amount === '') {
       return;
-    } else {
-      sendTransaction();
     }
+    sendTransaction();
+    return;
   };
   return (
     <div className="mainContainer">
-        <div className="cryptContainer">
-            <h1 className="title">Crypt Card</h1>
-            <button type="button">
-                <p className="buttonText" onClick={connectWallet}>ウォレット連携</p>
-            </button>
-        </div>
-        {/** 右側 */}
-        <div className="inputContainer">
-            <input type="text" placeholder="アドレス" name="addressTo" onChange={(e) => handleChange(e, "addressTo")} />
-            <input type="number" placeholder="通貨(ETH)" name="amount" step="0.0001" onChange={(e) => handleChange(e, "amount")} />
-            <button type="button" onClick={handleSubmit}>送信</button>
-        </div>
+      <div className="cryptContainer">
+        <h1 className="title">Crypt Card</h1>
+        <button type="button">
+          <p className="buttonText" onClick={connectWallet}>ウォレット連携</p>
+        </button>
+      </div>
+      {/** 右側 */}
+      <div className="inputContainer">
+        <input type="text" placeholder="アドレス" name="addressTo" onChange={(e) => handleChange(e, 'addressTo')} />
+        <input type="number" placeholder="通貨(ETH)" name="amount" step="0.0001" onChange={(e) => handleChange(e, 'amount')} />
+        <button type="button" onClick={handleSubmit}>送信</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Main;
--- client/src/context/TransactionContext.js
+++ client/src/context/TransactionContext.js
@@ -6,84 +6,87 @@ export const TransactionContext = createContext();
 
 // スマートコントラクトを取得
 const getSmartContract = () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const transactionContract = new ethers.Contract(contactAddress, contactABI, signer);
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(contactAddress, contactABI, signer);
 
-    return transactionContract;
+  return transactionContract;
 };
 
 // ウォレット連携など
 export const TransactionProvider = ({ children }) => {
-    const [crrentAccount, setCrrentAccount] = useState("");
-    const [inputFormData, setInputFormData] = useState({
-        addressTo: "",
-        amount: "",
+  const [crrentAccount, setCrrentAccount] = useState('');
+  const [inputFormData, setInputFormData] = useState({
+    addressTo: '',
+    amount: '',
+  });
+
+  const handleChange = (error, name) => {
+    setInputFormData((prevInputFromData) => ({
+      ...prevInputFromData,
+      [name]: error.target.value,
+    }));
+  };
+
+  const isMetamaskInstalled = () => {
+    if(!window.ethereum) return console.log('please metamask install');
+  };
+
+  // metamaskと連携しているか確認
+  const isMetamaskWalletConnect = async () => {
+    isMetamaskInstalled();
+
+    // metamaskのアカウントIDを取得
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    setCrrentAccount(accounts[0]);
+  };
+
+  // metamask wallet連携
+  const connectWallet = async () => {
+    isMetamaskInstalled();
+
+    // metamaskを持っていればコネクトを行う
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  };
+
+  // 実際に通貨の取引を行う
+  const sendTransaction = async () => {
+    isMetamaskInstalled();
+    const transactionContract = getSmartContract();
+    const { addressTo, amount } = inputFormData;
+    const parseAmount = ethers.utils.parseEther(amount)._hex;
+    const transactionParameters = {
+      gas: '0',
+      to: addressTo,
+      from: crrentAccount,
+      value: parseAmount,
+    };
+
+    const txHash = await window.ethereum.request({ // eslint-disable-line no-unused-vars
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
     });
 
-    const handleChange = (e, name) => {
-        setInputFormData((prevInputFromData) => ({
-            ...prevInputFromData,
-            [name]: e.target.value,
-        }));
-    };
+    // 送金者から受信者に対して通貨送金が可能
+    const transactionHash = await transactionContract.addToBlockchain(
+      addressTo,
+      parseAmount,
+    );
+    console.log(`ロード中・・・${transactionHash.txHash}`);
+    await transactionHash.wait();
+    console.log(`送金に成功！${transactionHash.txHash}`);
+  };
 
-    const isMetamaskInstalled = async () => {
-        if(!window.ethereum) return alert("please metamask install");
-    }
+  useEffect(() => {
+    isMetamaskWalletConnect();
+  }, []);
 
-    // metamaskと連携しているか確認
-    const isMetamaskWalletConnect = async () => {
-        isMetamaskInstalled();
-
-        // metamaskのアカウントIDを取得
-        const accounts = await window.ethereum.request({ method: "eth_accounts" });
-        setCrrentAccount(accounts[0]);
-    }
-
-    // metamask wallet連携
-    const connectWallet = async () => {
-        isMetamaskInstalled();
-
-        // metamaskを持っていればコネクトを行う
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-    }
-
-    // 実際に通貨の取引を行う
-    const sendTransaction = async () => {
-        isMetamaskInstalled();
-        const transactionContract = getSmartContract();
-        const { addressTo, amount } = inputFormData;
-        const parseAmount = ethers.utils.parseEther(amount)._hex;
-        const transactionParameters = {
-            gas: '0',
-            to: addressTo,
-            from: crrentAccount,
-            value: parseAmount,
-        };
-
-        const txHash = await window.ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [transactionParameters],
-        });
-
-        // 送金者から受信者に対して通貨送金が可能
-        const transactionHash = await transactionContract.addToBlockchain(
-            addressTo,
-            parseAmount
-        );
-        console.log(`ロード中・・・${transactionHash.txHash}`);
-        await transactionHash.wait();
-        console.log(`送金に成功！${transactionHash.txHash}`);
-    };
-
-    useEffect(() => {
-        isMetamaskWalletConnect();
-    }, [])
-
-    return (
-        <TransactionContext.Provider value={{ connectWallet, sendTransaction, handleChange, inputFormData }}>
-            { children }
-        </TransactionContext.Provider>
-    )
-}
+  return (
+    <TransactionContext.Provider value={{
+      connectWallet, sendTransaction, handleChange, inputFormData,
+    }}
+    >
+      { children }
+    </TransactionContext.Provider>
+  );
+};
